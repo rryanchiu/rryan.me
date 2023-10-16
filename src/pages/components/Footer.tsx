bc2c9050da896c06fa469b47cdaf27cae16bcd4b
Ryan Chiu <xiaoliang.qiu@outlook.com>
2023-10-17 01:50:36 +0800

    update

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -2,7 +2,7 @@ export default () => {
     return (
         <div className="mt-10">
             <footer className="text-gray-500">Ryan © 2023</footer>
-            <footer className="text-gray-500">Modified from <a className="text-gray hover:text-gray-700" href="https://ddiu.io/" target="_blank">ddiu.io</a>. Published under MIT Licence.</footer>
+            <footer className="text-gray-500">Modified from <a className="text-gray hover:text-gray-700" href="https://ddiu.io/" target="_blank">ddiu.io</a>. Published under the MIT Licence.</footer>
         </div>
     )
 }
--- src/pages/components/Project.tsx
+++ src/pages/components/Project.tsx
@@ -1,10 +1,16 @@
-
 import './Components.css'
 
 export default () => {
     const openLink = () => {
         window.open('https://github.com/rryanchiu', '_blank')
     }
+
+    const projects = [{
+        name: 'Smart Dalton💬',
+        icon: 'An simple web UI for ChatGPT',
+        url: 'https://chatgpt.ililili.me/',
+        description: 'An simple web UI for ChatGPT',
+    }]
     return (
         <>
             <h2 className="flex ">
@@ -13,9 +19,23 @@ export default () => {
                     onClick={openLink}
                     className="op-50 ml-2 hover:op-100 transition-opacity cursor-pointer"
                 >
-                    <div className="ri-arrow-right-up-line text-3xl" ></div>
+                    <div className="ri-arrow-right-up-line text-3xl"></div>
                 </div>
             </h2>
+            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2">
+                {projects.map((item, index) => (
+                    <div key={index} className={'rd-2 p-3 bg-[#f8f8f8] flex color-gray-8 gap-4 cursor-pointer hover:bg-dark-2 hover:color-gray-1'} onClick={() => {
+                        window.open(item.url)
+                    }}>
+                        <img src="/public/dalton.svg" alt="" className={'w-10'}/>
+
+                        <div className={' grid '}>
+                            <span className={'font-700'}>  {item.name}</span>
+                            <span className={' text-sm'}>  {item.description}</span>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
